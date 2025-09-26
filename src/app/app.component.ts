9d8fc48487792853c2822bef1206745a2103430c
UELISSON TIAGO SILVA SANTOS <020.732974@universidadesenaicimatec.edu.br>
2025-09-26 15:17:20 -0300

    Tentando conserta a nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './coponentes/menu/menu.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuComponent],
+  imports: [RouterOutlet, MenuComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
